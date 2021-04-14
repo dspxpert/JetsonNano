a69ff461bfe7c3c8695485852bd553787bb7b0c7
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-04-14 10:58:52 +0900

    Update keyboard3.py

--- MotorHAT/keyboard3.py
+++ MotorHAT/keyboard3.py
@@ -2,6 +2,9 @@
 
 import curses
 
+# curses key code definition
+# https://www.gnu.org/software/guile-ncurses/manual/html_node/Getting-characters-from-the-keyboard.html
+
 def main(stdscr):
     # do not wait for input when calling getch
     stdscr.nodelay(1)
@@ -10,7 +13,7 @@ def main(stdscr):
         c = stdscr.getch()
         if c != -1:
             # print numeric value
-            stdscr.clear()  # clear screen
+            stdscr.clear()
             stdscr.addstr(0,0, str(c) + ' ')
             stdscr.refresh()
             stdscr.move(0,0)
@@ -18,6 +21,26 @@ def main(stdscr):
             if c == ord('w'):
                 #print("hello")
                 stdscr.addstr(1,0, "hello")
+                stdscr.refresh()
+                stdscr.move(0,0)
+            if c == curses.KEY_UP:
+                #print("hello")
+                stdscr.addstr(1,0, "up")
+                stdscr.refresh()
+                stdscr.move(0,0)
+            if c == curses.KEY_DOWN:
+                #print("hello")
+                stdscr.addstr(1,0, "down")
+                stdscr.refresh()
+                stdscr.move(0,0)
+            if c == curses.KEY_LEFT:
+                #print("hello")
+                stdscr.addstr(1,0, "left")
+                stdscr.refresh()
+                stdscr.move(0,0)
+            if c == curses.KEY_RIGHT:
+                #print("hello")
+                stdscr.addstr(1,0, "right")
                 stdscr.refresh()
                 stdscr.move(0,0)
             if c == 3 or c == ord('q') or c == ord(' '):
