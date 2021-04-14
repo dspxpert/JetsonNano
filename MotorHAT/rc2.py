f9943eda685a8b2277c9cf5cff401acde865feae
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-04-14 10:06:03 +0900

    Add files via upload

---
+++ MotorHAT/rc2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import curses
+from PCA9685 import PCA9685
+import time
+Dir = [
+    'forward',
+    'backward',
+]
+pwm = PCA9685(0x40, debug=True)
+pwm.setPWMFreq(50)
+class MotorDriver():
+    def __init__(self):
+        self.PWMA = 0
+        self.AIN1 = 1
+        self.AIN2 = 2
+        self.PWMB = 5
+        self.BIN1 = 3
+        self.BIN2 = 4
+
+    def MotorRun(self, motor, index, speed):
+        if speed > 100:
+            return
+        if(motor == 0):
+            pwm.setDutycycle(self.PWMA, speed)
+            if(index == Dir[0]):
+                pwm.setLevel(self.AIN1, 0)
+                pwm.setLevel(self.AIN2, 1)
+            else:
+                pwm.setLevel(self.AIN1, 1)
+                pwm.setLevel(self.AIN2, 0)
+        else:
+            pwm.setDutycycle(self.PWMB, speed)
+            if(index == Dir[0]):
+                pwm.setLevel(self.BIN1, 1)
+                pwm.setLevel(self.BIN2, 0)
+            else:
+                pwm.setLevel(self.BIN1, 0)
+                pwm.setLevel(self.BIN2, 1)
+
+    def MotorStop(self, motor):
+        if (motor == 0):
+            pwm.setDutycycle(self.PWMA, 0)
+        else:
+            pwm.setDutycycle(self.PWMB, 0)
+
+
+
+def main(stdscr):
+    Motor = MotorDriver()
+    speed = 100
+    # do not wait for input when calling getch
+    stdscr.nodelay(1)
+    while True:
+        # get keyboard input, returns -1 if none available
+        c = stdscr.getch()
+        if c != -1:
+            # print numeric value
+            stdscr.addstr(str(c) + ' ')
+            stdscr.refresh()
+            # return curser to start position
+            stdscr.move(0, 0)
+            if c == 119 or c == 259: # w or up key
+                print('forward')
+                Motor.MotorRun(0, 'forward', speed)
+                Motor.MotorRun(1, 'forward', speed)
+            if c == 115 or c == 258: # s or down key
+                print('backward')
+                Motor.MotorRun(0, 'backward', speed)
+                Motor.MotorRun(1, 'backward', speed)
+            if c == 97 or c == 260:  # a or left key
+                print('left')
+                Motor.MotorRun(0, 'forward', 0)
+                Motor.MotorRun(1, 'forward', speed)
+            if c == 100 or c == 261: # d or right key
+                print('right')
+                Motor.MotorRun(0, 'forward', speed)
+                Motor.MotorRun(1, 'forward', 0)
+            if c == 113 or c == 32: # q or space key
+                print('stop')
+                Motor.MotorRun(0, 'forward', 0)
+                Motor.MotorRun(1, 'forward', 0)
+            if c == 3:              # ctrl-c key
+                print('bye')
+                Motor.MotorRun(0, 'forward', 0)
+                Motor.MotorRun(1, 'forward', 0)
+                break
+
+if __name__ == '__main__':
+    curses.wrapper(main)
+
+
